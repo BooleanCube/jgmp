1d904052b88ec3a3a58bad9a4be5ae1bb653a7cc
boole <johnnyfastfigners1234@gmail.com>
2022-08-09 09:37:44 -0400

    gravity documentation

--- src/main/java/com/boole/jgmp/physics/JGMPGravity.java
+++ src/main/java/com/boole/jgmp/physics/JGMPGravity.java
@@ -2,11 +2,29 @@ package com.boole.jgmp.physics;
 
 import com.boole.jgmp.math.vectors.JGMPVector2;
 
+/**
+ * Static Gravity Force Model for Universal Gravity. <br>
+ * If you plan to use more than 1 Gravity Model for your application, you will have to create your
+ * own Static Gravity Model for each different one.
+ */
 public class JGMPGravity {
 
+    /**
+     * Static Float Acceleration for Universal Gravity Mode. <br>
+     * Default acceleration = 9.8 m/s^2.
+     */
     public static float acceleration = 9.8f;
+
+    /**
+     * Universal Static Gravity Force Model that uses the acceleration set. <br>
+     * Default acceleration = 9.8 m/s^2
+     */
     public final static JGMPForce gravity = new JGMPForce(JGMPVector2.DOWN, acceleration);
 
+    /**
+     * Static function to set the acceleration value for all the objects in this Gravity Field.
+     * @param acceleration new acceleration float value
+     */
     public static void setAcceleration(float acceleration) {
         JGMPGravity.acceleration = acceleration;
         JGMPGravity.gravity.force = JGMPGravity.acceleration;
