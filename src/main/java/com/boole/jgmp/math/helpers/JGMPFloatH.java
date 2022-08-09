fd54500d23f1208ae772368d95ac0d3c2966e348
boole <johnnyfastfigners1234@gmail.com>
2022-08-08 23:56:54 -0400

    float helper documentation

--- src/main/java/com/boole/jgmp/math/helpers/JGMPFloatH.java
+++ src/main/java/com/boole/jgmp/math/helpers/JGMPFloatH.java
@@ -6,14 +6,56 @@ public class JGMPFloatH {
 
     private static float epsilon = 0.0001f;
 
+    /**
+     * Minimum between 2 floats
+     * @param a float
+     * @param b float
+     * @return minimum of the 2 floats
+     */
     public static float min(float a, float b) { return Math.min(a, b); }
+
+    /**
+     * Maximum between 2 floats
+     * @param a float
+     * @param b float
+     * @return maximum of the 2 floats
+     */
     public static float max(float a, float b) { return Math.max(a, b); }
 
+    /**
+     * Clamps a float between the min and max
+     * @param v float value
+     * @param min min float capacity
+     * @param max max float capacity
+     * @return clamped float value
+     */
     public static float clamp(float v, float min, float max) { return v > max ? max : max(v,min); }
-    public static float lerp(float initial, float ending, float t) { return (ending - initial) * t + initial; }
-    public static float inverseLerp(float initial, float ending, float v) { return (v - initial) / (ending - initial); }
 
-    // Cubic Bezier Curve Interpolation
+    /**
+     * Lerping floats given start and end
+     * @param initial starting point float
+     * @param ending ending point float
+     * @param t float t
+     * @return lerped float value
+     */
+    public static float lerp(float initial, float ending, float t) { return (ending - initial) * t + initial; }
+
+    /**
+     * Inverse Lerping floats given start and end
+     * @param initial starting point float
+     * @param ending ending point float
+     * @param t float t
+     * @return inversely lerped float value
+     */
+    public static float inverseLerp(float initial, float ending, float t) { return (t - initial) / (ending - initial); }
+
+    /**
+     * 1D Cubic Bezier Curve Interpolation
+     * @param initial starting point
+     * @param ending ending point
+     * @param r ratio float
+     * @return interp float value
+     */
     public static float bezierInterp1D(JGMPVector2 initial, JGMPVector2 ending, float r) {
         JGMPVector2[] points = new JGMPVector2[]{
                 JGMPVector2.ZERO,
@@ -25,6 +67,13 @@ public class JGMPFloatH {
 
         return point.dot(JGMPVector2.UP);
     }
+
+    /**
+     * 2D Cubic Bezier Curve Interpolation
+     * @param points array of points
+     * @param r ratio float
+     * @return interp float value
+     */
     public static JGMPVector2 bezierInterp2D(JGMPVector2[] points, float r) {
         final int nump = 4;
 
@@ -41,26 +90,70 @@ public class JGMPFloatH {
         return tempbuf[0];
     }
 
+    /**
+     * Remapping float values
+     * @param fval float value
+     * @param fmin min float capacity
+     * @param fmax max float capacity
+     * @param tmin t min float capacity
+     * @param tmax t max float capacity
+     * @return
+     */
     public static float remap(float fval, float fmin, float fmax, float tmin, float tmax) {
         float t = inverseLerp(fmin, fmax, fval);
         return lerp(tmin, tmax, t);
     }
+
+    /**
+     * Checks whether 2 floats are approximately equal with a relative error of the epsilon.
+     * @param a first float value to compare
+     * @param b second float value to compare
+     * @return boolean indicating if the 2 floats are approximately equal.
+     */
     public static boolean approxEqual(float a, float b) { return Math.abs(a-b) <= epsilon; }
 
+    /**
+     * Sign of the given float
+     * @param v float value
+     * @return sign float value of <b>v</b>
+     */
     public static float sign(float v) { return Float.compare(v, 0); }
 
+    /**
+     * Rounds the given float value to a near multiple. <br>
+     * This is not entirely accurate, but the algorithm works accurate enough.
+     * @param v float value
+     * @param mult float multiple
+     * @return rounded multiple
+     */
     public static float roundToMultiple(float v, float mult) {
         v += mult/2f;
         float r = v%mult;
         if(v > 0f) return v+mult-r;
         return v-r;
     }
+
+    /**
+     * Rounds the given float value up to a near multiple. <br>
+     * This is not entirely accurate, but the algorithm works accurate enough.
+     * @param v float value
+     * @param mult float multiple
+     * @return rounded up multiple
+     */
     public static float roundUpToMultiple(float v, float mult) {
         float r = v%mult;
         if(r == 0f) return v;
         if(v > 0f) return v+mult-r;
         return v-r;
     }
+
+    /**
+     * Rounds the given float value down to a near multiple. <br>
+     * This is not entirely accurate, but the algorithm works accurate enough.
+     * @param v float value
+     * @param mult float multiple
+     * @return rounded down multiple
+     */
     public static float roundDownToMultiple(float v, float mult) {
         float r = v%mult;
         if(r == 0f) return v;
